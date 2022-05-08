428c7f8478685d943ae250db05b4fc62bc99a7b4
Michael Bestas <mkbestas@gmail.com>
2022-05-08 03:45:18 +0300

    raviole: Add privapp permissions for HbmSVManager
    Change-Id: Ieab465f478424d8a936519250b40832f1c6bbc33

--- device-oriole.mk
+++ device-oriole.mk
@@ -18,6 +18,10 @@ include device/google/gs101/common.mk
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
 
+# HBM
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
+
 # Overlays
 PRODUCT_PACKAGES += \
     DeviceAsWebcamRaviole \
