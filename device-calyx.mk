da728427db7c7ac78c953773d1a349978643d680
Nolen Johnson <johnsonnolen@gmail.com>
2022-05-18 00:13:03 +0530

    Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,5 +1,10 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/raviole/overlay-calyx
 
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
