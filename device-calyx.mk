f99343a1a9ac6d0df65da3b4074d659b5a94b298
Michael Bestas <mkbestas@gmail.com>
2022-08-25 19:56:19 +0300

    Move PRODUCT_PACKAGE_OVERLAYS before vendor/calyx inclusion
    This fixes config_appsNotReportingCrashes being overwritten
    by vendor/calyx overlays.
    
    Change-Id: Ib593840f28a62c03fc8f6f94af26479f44e6f55f
    (cherry picked from commit fb19afb8a2915ad1e68b039d0f646aa19d14cff9)

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,5 +1,3 @@
-DEVICE_PACKAGE_OVERLAYS += device/google/raviole/overlay-calyx
-
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
