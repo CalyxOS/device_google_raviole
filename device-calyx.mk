8857494987db0818f28ab52e0b241af1834ca8d4
Tommy Webb <tommy@calyxinstitute.org>
2023-12-07 15:47:13 +0000

    Add privapp permissions for PixelCameraServices
    Change-Id: I5862120c01b38c1fd0e78b7b13ec205de8fea4c3

--- device-calyx.mk
+++ device-calyx.mk
@@ -13,6 +13,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/google/raviole/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 
+# Pixel Camera Services / Camera extensions
+PRODUCT_COPY_FILES += \
+    device/google/pantah/permissions/permissions_com.google.android.apps.camera.services.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.apps.camera.services.xml
+
 # HBM
 PRODUCT_COPY_FILES += \
     device/google/raviole/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
