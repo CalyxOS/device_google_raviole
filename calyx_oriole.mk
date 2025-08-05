9b599877cfccbc228396d329878867977659a13e
Michael Bestas <mkbestas@gmail.com>
2025-08-05 21:17:29 +0000

    Update fingerprint/build description from BP2A.250605.031.A5
    Change-Id: Ib9926c9939106974a9b2ac6d0f489bbbdfab6f69

--- calyx_oriole.mk
+++ calyx_oriole.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel 6
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="oriole-user 16 BP2A.250605.031.A2 13578606 release-keys" \
-    BuildFingerprint=google/oriole/oriole:16/BP2A.250605.031.A2/13578606:user/release-keys \
+    BuildDesc="oriole-user 16 BP2A.250605.031.A5 13701493 release-keys" \
+    BuildFingerprint=google/oriole/oriole:16/BP2A.250605.031.A5/13701493:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
--- calyx_raven.mk
+++ calyx_raven.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel 6 Pro
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="raven-user 16 BP2A.250605.031.A2 13578606 release-keys" \
-    BuildFingerprint=google/raven/raven:16/BP2A.250605.031.A2/13578606:user/release-keys \
+    BuildDesc="raven-user 16 BP2A.250605.031.A5 13701493 release-keys" \
+    BuildFingerprint=google/raven/raven:16/BP2A.250605.031.A5/13701493:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
