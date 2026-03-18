e2b7dcfce7417e21755d3386c3d7f655f06394b6
Michael Bestas <mkbestas@gmail.com>
2026-03-18 03:09:47 +0200

    Initial CalyxOS makefiles
    Change-Id: I0aeb85302967c3ce004f7f18a967813684783088

---
+++ calyx_oriole.mk
@@ -0,0 +1,28 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common stuff
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
+
+# Inherit device configuration
+DEVICE_CODENAME := oriole
+DEVICE_PATH := device/google/raviole
+VENDOR_PATH := vendor/google/oriole
+$(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 6
+PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="oriole-user 16 BP4A.251205.006 14401865 release-keys" \
+    BuildFingerprint=google/oriole/oriole:16/BP4A.251205.006/14401865:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
